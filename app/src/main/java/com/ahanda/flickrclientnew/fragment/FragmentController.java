76f219c8dbeca207fc84ab6f3996bd0bad9ccde5
Abhi Handa <ahanda@ebay.com>
2019-02-11 21:40:13 -0800

    Adding Fragment

---
+++ app/src/main/java/com/ahanda/flickrclientnew/fragment/FragmentController.java
@@ -0,0 +1,28 @@
+package com.ahanda.flickrclientnew.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ahanda.flickrclientnew.R;
+
+public class FragmentController extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.recycler_fragment, container);
+
+    }
+
+
+
+
+
+}
