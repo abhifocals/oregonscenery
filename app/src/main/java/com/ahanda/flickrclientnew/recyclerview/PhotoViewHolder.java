568b6a5f8bc3344ecccadcc36592c73825b5d361
Abhi Handa <ahanda@ebay.com>
2019-02-16 16:06:04 -0800

    Starting new activity when photo clicked.

--- app/src/main/java/com/ahanda/flickrclientnew/recyclerview/PhotoViewHolder.java
+++ app/src/main/java/com/ahanda/flickrclientnew/recyclerview/PhotoViewHolder.java
@@ -1,5 +1,6 @@
 package com.ahanda.flickrclientnew.recyclerview;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.ahanda.flickrclientnew.R;
+import com.ahanda.flickrclientnew.activity.PhotoActivity;
 import com.ahanda.flickrclientnew.events.PhotoClickEvent;
 import com.ahanda.flickrclientnew.rest.Photo;
 import com.squareup.picasso.Picasso;
@@ -36,6 +38,9 @@ public class PhotoViewHolder extends RecyclerView.ViewHolder implements View.OnC
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(view.getContext(), "Photo Clicked", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(view.getContext(), PhotoActivity.class);
+        intent.putExtra("currentPhoto", photo);
+
+        view.getContext().startActivity(intent);
     }
 }
