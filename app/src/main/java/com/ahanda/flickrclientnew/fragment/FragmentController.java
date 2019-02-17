87d189f511499ccc15c83a629f8e752297b01583
Abhi Handa <ahanda@ebay.com>
2019-02-16 16:05:37 -0800

    Removing redundant imports.

--- app/src/main/java/com/ahanda/flickrclientnew/fragment/FragmentController.java
+++ app/src/main/java/com/ahanda/flickrclientnew/fragment/FragmentController.java
@@ -4,19 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.ahanda.flickrclientnew.MainActivity;
 //import com.ahanda.flickrclientnew.R;
 import com.ahanda.flickrclientnew.R;
 import com.ahanda.flickrclientnew.events.FlickResponseEvent;
@@ -31,7 +27,6 @@ import org.greenrobot.eventbus.Subscribe;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import retrofit2.Retrofit;
 
 public class FragmentController extends Fragment {
 
