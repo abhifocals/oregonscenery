8655d631060023651eb5c9da31739fbb702f6032
Abhi Handa <ahanda@ebay.com>
2019-02-20 21:22:19 -0800

    Using Activity instead of AppCompat.

--- app/src/main/java/com/ahanda/flickrclientnew/activity/PhotoActivity.java
+++ app/src/main/java/com/ahanda/flickrclientnew/activity/PhotoActivity.java
@@ -1,5 +1,6 @@
 package com.ahanda.flickrclientnew.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +11,7 @@ import com.ahanda.flickrclientnew.R;
 import com.ahanda.flickrclientnew.rest.Photo;
 import com.squareup.picasso.Picasso;
 
-public class PhotoActivity extends AppCompatActivity {
+public class PhotoActivity extends Activity {
 
 
     @Override
